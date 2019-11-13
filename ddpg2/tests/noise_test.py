ac0099a2e1d0f7eb880189613ada0866330ec71a
szymon <szymon.brych@gmail.com>
2019-11-13 14:18:52 +0100

    Moving things around

--- ddpg2/tests/noise_test.py
+++ ddpg2/tests/noise_test.py
@@ -3,7 +3,7 @@ from ddpg2 import NormalNoise
 import numpy as np
 
 class NoiseTest(unittest.TestCase):
-    def apply_0_noise(self):
+    def test_apply_0_noise(self):
         n = NormalNoise(0)
         input = 2.5*np.ones(shape=(1,),dtype=np.float32)
         self.assertAlmostEqual(n.apply(input),2.5,delta=1e-8)
